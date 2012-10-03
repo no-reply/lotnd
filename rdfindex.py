a7c0228ff3e3f2488ed6d8ff676d2c267929ebc0
Thomas Johnson <thomas.johnson@oregonstate.edu>
2012-10-03 16:41:42 -0700

    use rdf:type

--- rdfindex.py
+++ rdfindex.py
@@ -12,6 +12,7 @@ endpoint.setReturnFormat(JSON)
 osuNs = rdflib.Namespace('http://data.library.oregonstate.edu/G/')
 outfile = open('theses.json', 'w')
 outfile_f = open('theses_f.json', 'w')
+outfile_e = open('theses_errors.json', 'w')
 mapping = json.loads(open('thesis.mapping.json', 'r').read())
 
 elastic = ES('http://data.library.oregonstate.edu:9200')
@@ -20,7 +21,7 @@ elastic.create_index('theses')
 elastic.put_mapping('thesis', {'properties':mapping}, ['theses'])
 
 # get list of theses
-listQuery = "SELECT ?thesis WHERE { ?thesis <http://www.w3.org/2000/01/rdf-schema#type> <http://purl.org/ontology/bibo/Thesis> . }"
+listQuery = "SELECT ?thesis WHERE { ?thesis <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/bibo/Thesis> . }"
 endpoint.setQuery(listQuery)
 theses = endpoint.query().convert()['results']['bindings']
 item_count = len(theses)
@@ -55,15 +56,18 @@ for thesis in theses:
             desc = endpoint.query().convert()
             tmpgraph.parse(data=desc.serialize(format='xml'))
 
-    try:
-        j = jsonld.compact(jsonld.from_rdf(g.serialize(format='nquads')), contexts)
-        outfile.write(json.dumps(j['@graph'][0], indent=1))
-        j = jsonld.frame(j, json.load(urllib2.urlopen('http://achelo.us/thesis_frame.jsonld')))
-        outfile_f.write(json.dumps(j['@graph'][0], indent=1))
-        elastic.index(j['@graph'][0], 'theses', 'thesis')
-    except Exception as e:
-        print e
-        continue
+    j = jsonld.compact(jsonld.from_rdf(g.serialize(format='nquads')), contexts)
+    if 0 in j['@graph']:
+        try:
+            outfile.write(json.dumps(j['@graph'][0], indent=1))
+            j = jsonld.frame(j, json.load(urllib2.urlopen('http://achelo.us/thesis_frame.jsonld')))
+            outfile_f.write(json.dumps(j['@graph'][0], indent=1))
+            elastic.index(j['@graph'][0], 'theses', 'thesis')
+        except Exception as e:
+            print e 
+            print json.dumps(j['@graph'])
+            outfile_e.write(json.dumps(j))
+            continue
 
     if (count % 100) == 0:
         print count
